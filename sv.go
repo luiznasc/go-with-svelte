2081d3ee526d5ddd6845cc1a0199ce5872af0598
luiznasc <luiz.nascimen@ccc.ufcg.edu.br>
2023-09-02 19:04:27 -0300

    fixing readme

---
+++ sv.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+	app.Static("/public", "./public")
+	app.Get("/", mainPage)
+	app.Listen(":3000")
+}
+
+func mainPage(c *fiber.Ctx) error {
+	return c.SendString("Hey")
+}
